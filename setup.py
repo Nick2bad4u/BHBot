ec2274e1f9b66ad32e2c861202d6a2f59c2d5128
Typpi <20943337+Nick2bad4u@users.noreply.github.com>
2024-03-19 15:56:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BHBot',
-    version='V1.2',
+    version='V1.3',
     packages=[''],
     url='https://github.com/Nick2bad4u/BHBot',
     license='Unlicense',
