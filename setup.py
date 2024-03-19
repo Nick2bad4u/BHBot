f15f3f55faf7afdcd8c33efc03511ce3f344e5e7
Typpi <20943337+Nick2bad4u@users.noreply.github.com>
2024-03-19 11:45:08 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BHBot-master',
+    version='V1',
+    packages=[''],
+    url='https://github.com/Nick2bad4u/BHBot',
+    license='Unlicense',
+    author='Nick',
+    author_email='',
+    description=''
+)
